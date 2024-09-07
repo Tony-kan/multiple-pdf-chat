a2acc908331ad7c1c0951c6a26df68aaa00a858f
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-09-07 17:53:47 +0200

    openai embeddings:vectorstore-FAISS

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 
 def get_pdf_text(pdf_docs):
@@ -22,7 +24,10 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
     
-
+def  get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore =FAISS.from_texts(texts=text_chunks,embedding=embeddings)
+    return vectorstore
 
 def main():
     load_dotenv()
@@ -41,9 +46,9 @@ def main():
 
                 # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
 
                 # create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
 if __name__  == "__main__":
     main()
