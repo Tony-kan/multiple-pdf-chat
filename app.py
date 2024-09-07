81d0b66bbceadfe9fd9fad3d1d11a37cc08dc78b
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-09-07 17:23:35 +0200

    extract text from all pdfs using PyPDF2

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader =  PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text +=  page.extract_text()
+    return text
 
 
 def main():
@@ -14,8 +24,9 @@ def main():
         pdf_docs = st.file_uploader("Upload your PDFs",accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-                pass
                 # get pdf text
+                raw_text =get_pdf_text(pdf_docs)
+                st.write(raw_text)
 
                 # get the text chunks
 
