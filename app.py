81fc79e45f1038c649c0be83a00d7af9bb868027
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-09-12 11:21:09 +0200

    display chat history

--- app.py
+++ app.py
@@ -53,6 +53,14 @@ def get_conversation_chain(vectorstore):
 
     # return rag_chain
     return conversation_chain
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question':user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i,message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
 
 
 def main():
@@ -65,9 +73,10 @@ def main():
         st.session_state.conversation = None
 
 
-
     st.header("Chat with multiple PDFs :books:")
-    st.text_input("Ask a question about your documents:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
 
     st.write(user_template.replace("{{MSG}}","Hello robot"),unsafe_allow_html=True)
     st.write(bot_template.replace("{{MSG}}","hello human"),unsafe_allow_html=True)
